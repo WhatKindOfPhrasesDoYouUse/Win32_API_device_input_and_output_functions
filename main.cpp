c4d50dcc6550c6492f33782fc646bcc37e050d7a
WhatKindOfPhrasesDoYouUse <115883000+WhatKindOfPhrasesDoYouUse@users.noreply.github.com>
2023-11-05 18:44:27 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -109,14 +109,14 @@ void task10() {
 
 int main() {
     connectRussian();
-    //task1();
-    //task2();
-    //task3();
-    //task4();
-    //task5();
-    //task6();
-    //task7();
-    //task8();
+    task1();
+    task2();
+    task3();
+    task4();
+    task5();
+    task6();
+    task7();
+    task8();
     task9();
     return 0;
 }
